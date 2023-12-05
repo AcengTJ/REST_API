545b297fc28bedcd4823cae082bf5daac07cb2a4
AcengTJ <acengtjhin69@gmail.com>
2023-12-05 13:43:42 +0700

    Make logout API and Logout Test case

--- REST-API/demo-api/src/main/java/com/domain/controller/AuthController.java
+++ REST-API/demo-api/src/main/java/com/domain/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.domain.controller;
 
+import com.domain.entity.User;
 import com.domain.model.LoginUserRequest;
 import com.domain.model.TokenResponse;
 import com.domain.model.WebResponse;
 import com.domain.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +26,14 @@ public class AuthController {
         TokenResponse tokenResponse = authService.login(request);
         return WebResponse.<TokenResponse>builder().data(tokenResponse).build();
 
+    }
+
+    @DeleteMapping(
+            path = "/api/auth/logout",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> logout(User user) {
+        authService.logout(user);
+        return WebResponse.<String>builder().data("Ok").build();
     }
 }
