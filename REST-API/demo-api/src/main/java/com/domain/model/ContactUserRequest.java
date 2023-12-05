1ab1e918db6e4fdad56c97776764141f0eb9b85a
AcengTJ <acengtjhin69@gmail.com>
2023-12-05 15:08:04 +0700

    Make ContactUserRequest

---
+++ REST-API/demo-api/src/main/java/com/domain/model/ContactUserRequest.java
@@ -0,0 +1,40 @@
+/**
+ * aniboys.id
+ * Copyright (c) 2017-2023 All Rights Reserved.
+ */
+package com.domain.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.crypto.Mac;
+
+/**
+ * @author Aceng <acengtjhin69@aniboys.id>
+ * @version $Id: ContactUserRequest.java, v 0.1 2023‐12‐05 15:04 Aceng Exp $$
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContactUserRequest {
+    @NotBlank
+    @Size(max = 100)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 100)
+    private String lastName;
+
+    @NotBlank
+    @Size(max = 100)
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
+    private String phone;
+}
