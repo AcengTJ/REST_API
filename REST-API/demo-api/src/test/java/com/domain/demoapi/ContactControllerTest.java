20534d0ee255f6b1d6f455050b378cebc8066277
AcengTJ <acengtjhin69@gmail.com>
2023-12-05 16:25:44 +0700

    Create createContactBadRequest test case

---
+++ REST-API/demo-api/src/test/java/com/domain/demoapi/ContactControllerTest.java
@@ -0,0 +1,95 @@
+/**
+ * aniboys.id
+ * Copyright (c) 2017-2023 All Rights Reserved.
+ */
+package com.domain.demoapi;
+
+import com.domain.entity.User;
+import com.domain.model.CreateContactRequest;
+import com.domain.repository.ContactRepository;
+import com.domain.repository.UserRepository;
+import com.domain.security.BCrypt;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import com.domain.entity.User;
+import com.domain.model.RegisterUserRequest;
+import com.domain.model.UpdateUserRequest;
+import com.domain.model.UserResponse;
+import com.domain.model.WebResponse;
+import com.domain.repository.UserRepository;
+import com.domain.security.BCrypt;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Objects;
+
+/**
+ * @author Aceng <acengtjhin69@aniboys.id>
+ * @version $Id: ContactControllerTest.java, v 0.1 2023‐12‐05 16:12 Aceng Exp $$
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ContactControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        contactRepository.deleteAll();
+        userRepository.deleteAll();
+
+        User user = new User();
+        user.setUsername("test");
+        user.setName("Test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        user.setToken("test");
+        user.setTokenExpiredAt(System.currentTimeMillis() + 10000000L);
+        userRepository.save(user);
+    }
+
+    @Test
+    void createContactBadRequest() throws Exception{
+        CreateContactRequest request = new CreateContactRequest();
+        request.setFirstName("");
+        request.setEmail("salah");
+
+        mockMvc.perform(
+                post("/api/contacts")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .header("X-API-TOKEN", "test")
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
+            });
+            assertNotNull(response.getErrors());
+        });
+    }
+}
