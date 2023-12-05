5ac964d373e0678f51a36e0c99e1e7f41ac956f6
AcengTJ <acengtjhin69@gmail.com>
2023-12-05 16:11:58 +0700

    Create ContactController

--- REST-API/demo-api/src/main/java/com/domain/controller/ContactController.java
+++ REST-API/demo-api/src/main/java/com/domain/controller/ContactController.java
@@ -1,8 +1,29 @@
 package com.domain.controller;
 
+import com.domain.entity.User;
+import com.domain.model.ContactResponse;
+import com.domain.model.CreateContactRequest;
+import com.domain.model.WebResponse;
+import com.domain.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContactController {
+    @Autowired
+    private ContactService contactService;
+
+    @PostMapping(
+            path = "/api/contacts",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<ContactResponse> create(User user, @RequestBody CreateContactRequest request) {
+        ContactResponse contactResponse = contactService.create(user, request);
+        return WebResponse.<ContactResponse>builder().data(contactResponse).build();
+    }
 
 }
